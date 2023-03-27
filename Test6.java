87b75e867a21ccdc000dbf39ef4c5dd835d388db
gandhamsandeepkumar8 <129029411+gandhamsandeepkumar8@users.noreply.github.com>
2023-03-27 10:50:42 +0530

    Add files via upload

---
+++ Test6.java
@@ -0,0 +1,10 @@
+class Test6
+{
+public static void main(String args[])
+{
+int a=10;
+int b=3;
+int c=a/b;
+System.out.println(a+"/"+b+"="+c);
+ }
+}
---
+++ program04.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Program04
+{
+public static void main(String args[])
+{
+Scanner scob=new Scanner(System.in);
+int num1=scob.nextInt();//15
+int num2=scob.nextInt();//3
+int i;//variable declaration
+i=0; //initialization 
+while(i<=num1)
+{
+             System.out.println(i+",");
+             i=i+num2;
+}//while loop
+
+}//main()
+}//program04
